ab94d5020fcbe7a411b824871db300c70b2c5428
joy <dongzhengyang@gmail.com>
2020-07-13 10:34:21 +0800

    add

--- database/migrations/2020_07_13_095305_create_cycling_records_table.php
+++ database/migrations/2020_07_13_095305_create_cycling_records_table.php
@@ -15,6 +15,17 @@ class CreateCyclingRecordsTable extends Migration
     {
         Schema::create('cycling_records', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('mongo_record_id',32)->default('')->comment('user_behavior._id')->index('mongo_record_id');
+            $table->unsignedInteger('start_time')->->default(0)->comment('开始时间');
+            $table->unsignedInteger('finish_time')->->default(0)->comment('结束时间');
+            $table->unsignedInteger('total_second')->->default(0)->comment('总时间s');
+            $table->float('total_distance',8,2)->default(0.00)->comment('总里程数');
+            $table->float('total_calories',8,2)->default(0.00)->comment('总卡路里');
+            $table->float('max_speed',8,2)->default(0.00)->comment('最大速度');
+            $table->float('avg_speed',8,2)->default(0.00)->comment('平均速度');
+            $table->float('avg_moving_speed',8,2)->default(0.00)->comment('平均移动速度');
+            $table->float('file_size',8,2)->default(0.00)->comment('flatbuffer文件大小 byte');
+            $table->string('file_url',150)->default('')->comment('文件地址');
             $table->timestamps();
         });
     }
