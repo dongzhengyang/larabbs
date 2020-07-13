1e0d1643ded583ac41a2d7d9898c1e12bc264ed3
joy <dongzhengyang@gmail.com>
2020-07-13 13:21:43 +0800

    test 5

--- app/Console/Commands/CyclingDataStatistics.php
+++ app/Console/Commands/CyclingDataStatistics.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Mongo\UserBehavior;
 use App\Models\Mongo\Cycling;
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\Query;
 use OSS\OssClient;
 use OSS\Core\OssException;
 use MongoDB\BSON\UTCDateTime;
@@ -43,10 +45,34 @@ class CyclingDataStatistics extends Command
      */
     public function handle()
     {
-        $datetime = (new UTCDateTime(1585670400*1000)->toDateTime();
-        $count =  UserBehavior::where('created_at','>=',$datetime)->count();
-        //$data = UserBehavior::first();
-        dd($count);
+        $datetime = (new UTCDateTime(1585670400*1000))->toDateTime();
+
+        $host = sprintf("mongodb://%s:%s@%s:%s/admin",
+            env('MONGO_DB_USERNAME'),
+            rawurlencode(env('MONGO_DB_PASSWORD')),
+            env('MONGO_DB_HOST', 'localhost'),
+            env('MONGO_DB_PORT', '27017'));
+
+        $manager = new Manager($host, ['socketTimeoutMS' => 900000]);
+        $query = new Query(
+            ['created_at' => ['$gt' => $datetime]]
+        );
+        $cursor = $manager->executeQuery('oplog.rs', $query);
+        $iterator = new \IteratorIterator($cursor);
+        $iterator->rewind();
+
+        while (true) {
+            if ($iterator->valid()) {
+                $document = ($iterator->current());
+                dd($document);
+            }
+        }
+
+
+
+//        $count =  UserBehavior::where('created_at','>=',$datetime)->count();
+//        //$data = UserBehavior::first();
+//        dd($count);
 
     }
 }
