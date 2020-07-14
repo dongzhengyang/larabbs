a8a0f6a94521b77b3fa5ed10e35ed923da4fc1af
joy <dongzhengyang@gmail.com>
2020-07-14 13:46:33 +0800

    ddd

--- app/Models/CyclingRecords.php
+++ app/Models/CyclingRecords.php
@@ -9,6 +9,11 @@ class CyclingRecords extends model
     protected $fillable = ['mongo_record_id', 'start_time', 'finish_time',
                             'total_second', 'total_distance', 'total_calories',
                             'max_speed','avg_speed','avg_moving_speed','file_size',
-                            'file_url'
+                            'file_url','app_version','os_version','phone_brand',
+                            'sportDuration','sportDistance','autoPauseDuration',
+                            'invalidDurationByDrift','invalidDistanceByDrift',
+                            'invalidAutoPauseDistanceByDrift','dozeDuration',
+                            'invalidDurationByHighSpeed','invalidDistanceByHighSpeed',
+                            'manualPauseDistance','autoPauseDistance','sportType'
                           ];
 }
