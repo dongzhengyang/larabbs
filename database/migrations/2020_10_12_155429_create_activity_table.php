fd7f2d2008bd5765ff41c3cede58abdbec869257
joy <dongzhengyang@gmail.com>
2020-10-12 16:25:51 +0800

    test

--- database/migrations/2020_10_12_155429_create_activity_table.php
+++ database/migrations/2020_10_12_155429_create_activity_table.php
@@ -15,6 +15,17 @@ class CreateActivityTable extends Migration
     {
         Schema::create('activity', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('activity_number')->unsigned()->index()->comment('活动编号');
+            $table->string('name', 100)->comment('活动名称');
+            $table->timestamp('time_begin')->nullable()->index()->comment('活动开始时间');
+            $table->timestamp('time_end')->nullable()->comment('活动结束时间');
+            $table->integer('part_count')->unsigned()->default(0)->comment('报名人数');
+            $table->integer('activity_count')->unsigned()->default(0)->comment('点亮活动勋章人数');
+            $table->integer('topic_count')->unsigned()->default(0)->comment('点亮主题勋章人数');
+            $table->string('mongo_activity_id', 32)->comment('mongo 活动id');
+            $table->unique('mongo_activity_id');
+            $table->timestamp('create_time')->nullable()->comment('mongo 创建时间');
+            $table->timestamp('update_time')->nullable()->comment('mongo 更新时间');
             $table->timestamps();
         });
     }
--- database/migrations/2020_10_12_155716_create_activity_participants_table.php
+++ database/migrations/2020_10_12_155716_create_activity_participants_table.php
@@ -15,6 +15,16 @@ class CreateActivityParticipantsTable extends Migration
     {
         Schema::create('activity_participants', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('activity_id')->unsigned()->comment('mysql活动编号');
+            $table->foreign('activity_id')->references('id')->on('activity');
+            $table->integer('activity_number')->unsigned()->comment('鼎连的活动编号');
+            $table->integer('dline_user_id')->unsigned()->comment('鼎连用户ID');
+            $table->integer('light_activity')->unsigned()->default(0)->comment('是否点亮活动勋章');
+            $table->integer('light_topic')->unsigned()->default(0)->comment('是否点亮主题勋章');
+            $table->timestamp('create_time')->nullable()->comment('mongo 创建时间');
+            $table->timestamp('update_time')->nullable()->comment('mongo 更新时间');
+            $table->timestamps();
+            $table->index(['activity_id', 'dline_user_id'],'index_activity_dline_user');
             $table->timestamps();
         });
     }
